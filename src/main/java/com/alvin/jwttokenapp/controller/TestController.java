3d3561f91a0df5c79f393b2c7251b9c30b6f13e0
alvin tan <alvintan95@gmail.com>
2022-01-23 11:51:21 +0800

    Add authorization to test controller

--- src/main/java/com/alvin/jwttokenapp/controller/TestController.java
+++ src/main/java/com/alvin/jwttokenapp/controller/TestController.java
@@ -1,7 +1,10 @@
 package com.alvin.jwttokenapp.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
@@ -9,10 +12,14 @@ import java.util.HashMap;
 import java.util.Map;
 
 @Controller
+@Slf4j
 public class TestController {
 
+    @PreAuthorize("hasAnyAuthority('/ADMIN/READ', '/APP/READ')")
     @GetMapping("/test")
     public ResponseEntity<?> test() {
+
+        log.info(String.valueOf(SecurityContextHolder.getContext().getAuthentication().getAuthorities()));
         Map<String, String> testMap = new HashMap<>();
         testMap.put("1", "A");
         testMap.put("2", "B");
