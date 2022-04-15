337010b7fdd8f5e7fcd5eb70915b8860463e2937
alvin tan <alvintan95@gmail.com>
2022-04-15 23:09:12 +0800

    add more data sql

--- src/main/resources/data-pg.sql
+++ src/main/resources/data-pg.sql
@@ -13,3 +13,17 @@ values ('/ADMIN/READ'),
        ('/APP/WRITE'),
        ('/APP/UPDATE'),
        ('/APP/READ');
+
+insert into app_role_authority (role_id, authority_id)
+values (1, 1),
+       (1, 2),
+       (1, 3),
+       (1, 4),
+       (1, 5),
+       (1, 6),
+       (1, 7),
+       (1, 8),
+       (2, 5),
+       (2, 6),
+       (2, 7),
+       (2, 8)
